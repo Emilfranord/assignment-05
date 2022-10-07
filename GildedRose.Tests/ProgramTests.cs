48f5e2aeab39c06c0f8b10b8553fc99f81e6813a
emno <emno@itu.dk>
2022-10-07 13:17:14 +0200

    add unit test for non-negative statement

--- GildedRose.Tests/ProgramTests.cs
+++ GildedRose.Tests/ProgramTests.cs
@@ -7,4 +7,48 @@ public class ProgramTests
     {
         true.Should().BeTrue();
     }
+
+    [Fact]
+    public void QualityAndSellecreasesForShirt()
+    {
+        var app = new Program(){
+            Items = new List<Item>{
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 }
+            }
+        };
+
+        app.UpdateQuality();
+
+        app.Items[0].Quality.Should().Be(19);
+        app.Items[0].SellIn.Should().Be(9);
+    }
+
+    [Fact]
+    public void QualityNonNegative()
+    {
+        var app = new Program(){
+            Items = new List<Item>{
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 1 }
+            }
+        };
+
+        app.UpdateQuality();
+        app.Items[0].Quality.Should().Be(0);
+    }
+
+    [Fact]
+    public void QualityNonNegativeRepeatedDecreasesAlsoKeepItNonNegative()
+    {
+        var app = new Program(){
+            Items = new List<Item>{
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 1 }
+            }
+        };
+
+        app.UpdateQuality();
+        app.UpdateQuality();
+        app.UpdateQuality();
+        app.Items[0].Quality.Should().Be(0);
+    }
+    
 }
